eb62ab8fe60e15b0cb8f2eb372e84d65f8e0a99c
G1R0es <maks.abubakirov.05@mail.ru>
2023-05-26 21:20:29 +0300

    tirst

---
+++ 3 task/script.js
@@ -0,0 +1,4 @@
+let num = prompt("Введите любое число");
+
+console.log(`Следующее число для ${num} - ${Number(num) + 1} `);
+console.log(`Предыдущее число для ${num} - ${Number(num) - 1} `);
---
+++ 4 task/script.js
@@ -0,0 +1,4 @@
+let firstNumber = Number(prompt("Введите любое число"));
+let lastNumber = Number(prompt("Введите второе число"));
+
+console.log(`${firstNumber} + ${lastNumber} = ${firstNumber + lastNumber}`);
